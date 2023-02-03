1f798d688eabd7708d15cc50acdd4547d6b61f46
Abdulhamid PAKYOL <pakyol.tc@gmail.com>
2023-02-03 20:53:22 +0300

    print Test class again

--- src/Test03022023/Test03022023.java
+++ src/Test03022023/Test03022023.java
@@ -3,6 +3,7 @@ package Test03022023;
 public class Test03022023 {
     public static void main(String[] args) {
         System.out.println("Test03022023");
+        System.out.println("Test03022023");
 
     }
 }
